a8490837d114b52e1b94b0d5f6a358af80481b61
Aljimpo <aljimpa@gmail.com>
2018-11-15 19:42:51 +0100

    Update pruebaVMP.c

--- pruebaVMP.c
+++ pruebaVMP.c
@@ -5,38 +5,40 @@
 
 #define VECES_PESO 4
 
-void pesosMes(double* pesomes){
-scanf("%lf" "%lf" "%lf" "%lf",&pesomes[0],&pesomes[1],&pesomes[2],&pesomes[3]);
+void pesosMes(double pesomes[]){
+int i;
+for(i=0;i<VECES_PESO;i++){
+    printf("Introduzca el peso de la semana %i: ",i);
+    scanf("%lf",&pesomes[i]);
+}
+
 }
 
 void main (void){
     double pesomes[VECES_PESO];
+    double pesomax,pesomin;
     pesosMes(pesomes);
-    pesoMinMax(pesomes);
+    pesoMinMax(pesomes,&pesomin,&pesomax);
+
+
+    printf("Peso maximo %f y minimo %f \n",pesomax,pesomin);
+    printf("Peso primera semana %f y peso ultima semana %f\n",pesomes[0],pesomes[3]);
+    printf("La diferencia de peso entre la primera y la ultima semana es: %f kilogramos",pesomes[0]-pesomes[3]);
 }
 
-void pesoMinMax (double *pesomes, double *peso_minn, double *peso_maxx){
+void pesoMinMax (double pesomes[], double *pesomin, double *pesomax){
 
-    double pesomin;
-    double pesomax;
-
-    peso_minn = &pesomes[0];
-    peso_maxx = &pesomes[0];
-
-    pesomin = *peso_minn;
-    pesomax = *peso_maxx;
-
+     *pesomin = *pesomes;
+     *pesomax = *pesomes;
 
     int i;
-    for(i=0;i<sizeof(pesomes);i++){
-        if (pesomes[i] < pesomin){
-            pesomin = pesomes[i];
+    for(i=0;i<VECES_PESO;i++){
+        if (pesomes[i] < *pesomin){
+            *pesomin = pesomes[i];
         }
-        if (pesomes[i] > pesomax){
-            pesomax = pesomes[i];
+        if (pesomes[i] > *pesomax){
+            *pesomax = pesomes[i];
         }
     }
-    printf("El peso min es:%f\n",pesomin);
-    printf("El peso max es:%f\n",pesomax);
 }
 
