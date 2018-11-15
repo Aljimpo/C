bd7f51296801575d29630ab6a74762021168a648
Aljimpo <aljimpa@gmail.com>
2018-11-15 21:48:27 +0100

    Add files via upload

--- pruebaVMP.c
+++ pruebaVMP.c
@@ -1,44 +1,43 @@
 #include <stdio.h>
 #include <string.h>
 
-/* Definicion de constantes */
+double leerDato (double min, double max){
+double dato;
 
-#define VECES_PESO 4
+    do{
+        printf("Introduzca un valor real comprendido entre %f y %f: ",min,max);
+        scanf("%lf", &dato);
 
-void pesosMes(double pesomes[]){
-int i;
-for(i=0;i<VECES_PESO;i++){
-    printf("Introduzca el peso de la semana %i: ",i);
-    scanf("%lf",&pesomes[i]);
-}
-
-}
-
-void main (void){
-    double pesomes[VECES_PESO];
-    double pesomax,pesomin;
-    pesosMes(pesomes);
-    pesoMinMax(pesomes,&pesomin,&pesomax);
-
-
-    printf("Peso maximo %f y minimo %f \n",pesomax,pesomin);
-    printf("Peso primera semana %f y peso ultima semana %f\n",pesomes[0],pesomes[3]);
-    printf("La diferencia de peso entre la primera y la ultima semana es: %f kilogramos",pesomes[0]-pesomes[3]);
-}
-
-void pesoMinMax (double pesomes[], double *pesomin, double *pesomax){
-
-     *pesomin = *pesomes;
-     *pesomax = *pesomes;
-
-    int i;
-    for(i=0;i<VECES_PESO;i++){
-        if (pesomes[i] < *pesomin){
-            *pesomin = pesomes[i];
+        if(min<=dato && dato<=max){
+            return dato;
         }
-        if (pesomes[i] > *pesomax){
-            *pesomax = pesomes[i];
+        else{
+            printf("ERROR: Valor fuera de rango");
         }
     }
+    while(min<dato || dato<max);
+
 }
 
+double riesgo (double ldl, double hdl, char* hayRiesgo){
+    double indiceRiesgo;
+    indiceRiesgo = ldl/hdl;
+    char positivo[] ="Hay riesgo cardiovascular ya que el indice de riesgo es";
+    char negativo[] ="No hay riesgo cardiovascular ya que el indice de riesgo es";
+    int i;
+    if (indiceRiesgo>3){
+        strcpy(hayRiesgo,positivo);
+    }else{
+        strcpy(hayRiesgo,negativo);
+    }
+    return indiceRiesgo;
+}
+
+void main(){
+    printf("Introduzca el valor de LDL y HDL\n");
+    char hayRiesgo[100];
+    double ldl=leerDato(100,220);
+    double hdl=leerDato(20,80);
+    double valorRiesgo = riesgo(ldl,hdl,hayRiesgo);
+    printf("%s %f (limite 3)",hayRiesgo,valorRiesgo);
+}
