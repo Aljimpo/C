0dfac6d23c68d849013e5e1a7b32bafdc80afb2e
Aljimpo <aljimpa@gmail.com>
2018-11-15 21:53:49 +0100

    Update pruebaVMP.c

--- pruebaVMP.c
+++ pruebaVMP.c
@@ -1,43 +1,55 @@
-#include <stdio.h>
-#include <string.h>
-
-double leerDato (double min, double max){
-double dato;
-
-    do{
-        printf("Introduzca un valor real comprendido entre %f y %f: ",min,max);
-        scanf("%lf", &dato);
-
-        if(min<=dato && dato<=max){
-            return dato;
-        }
-        else{
-            printf("ERROR: Valor fuera de rango");
-        }
-    }
-    while(min<dato || dato<max);
+void pesosMes(double* pesomes){
+scanf("%lf" "%lf" "%lf" "%lf",&pesomes[0],&pesomes[1],&pesomes[2],&pesomes[3]);
+void pesosMes(double pesomes[]){
+int i;
+for(i=0;i<VECES_PESO;i++){
+    printf("Introduzca el peso de la semana %i: ",i);
+    scanf("%lf",&pesomes[i]);
+}
 
 }
 
-double riesgo (double ldl, double hdl, char* hayRiesgo){
-    double indiceRiesgo;
-    indiceRiesgo = ldl/hdl;
-    char positivo[] ="Hay riesgo cardiovascular ya que el indice de riesgo es";
-    char negativo[] ="No hay riesgo cardiovascular ya que el indice de riesgo es";
+void main (void){
+    double pesomes[VECES_PESO];
+    double pesomax,pesomin;
+    pesosMes(pesomes);
+    pesoMinMax(pesomes);
+}
+    pesoMinMax(pesomes,&pesomin,&pesomax);
+
+void pesoMinMax (double *pesomes, double *peso_minn, double *peso_maxx){
+
+    double pesomin;
+    double pesomax;
+
+    peso_minn = &pesomes[0];
+    peso_maxx = &pesomes[0];
+    printf("Peso maximo %f y minimo %f \n",pesomax,pesomin);
+    printf("Peso primera semana %f y peso ultima semana %f\n",pesomes[0],pesomes[3]);
+    printf("La diferencia de peso entre la primera y la ultima semana es: %f kilogramos",pesomes[0]-pesomes[3]);
+}
+
+    pesomin = *peso_minn;
+    pesomax = *peso_maxx;
+void pesoMinMax (double pesomes[], double *pesomin, double *pesomax){
+
+     *pesomin = *pesomes;
+     *pesomax = *pesomes;
+
     int i;
-    if (indiceRiesgo>3){
-        strcpy(hayRiesgo,positivo);
-    }else{
-        strcpy(hayRiesgo,negativo);
+    for(i=0;i<sizeof(pesomes);i++){
+        if (pesomes[i] < pesomin){
+            pesomin = pesomes[i];
+    for(i=0;i<VECES_PESO;i++){
+        if (pesomes[i] < *pesomin){
+            *pesomin = pesomes[i];
+        }
+        if (pesomes[i] > pesomax){
+            pesomax = pesomes[i];
+        if (pesomes[i] > *pesomax){
+            *pesomax = pesomes[i];
+        }
     }
-    return indiceRiesgo;
-}
-
-void main(){
-    printf("Introduzca el valor de LDL y HDL\n");
-    char hayRiesgo[100];
-    double ldl=leerDato(100,220);
-    double hdl=leerDato(20,80);
-    double valorRiesgo = riesgo(ldl,hdl,hayRiesgo);
-    printf("%s %f (limite 3)",hayRiesgo,valorRiesgo);
+    printf("El peso min es:%f\n",pesomin);
+    printf("El peso max es:%f\n",pesomax);
 }
